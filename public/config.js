d127159b0b2abcdde2333bcf9dfb130f4a6c1802
Mario Reder <mario.reder@pm.me>
2025-09-28 22:53:31 +0200

    Update DEX configuration and branding

--- public/config.js
+++ public/config.js
@@ -25,7 +25,7 @@ window.__RUNTIME_CONFIG__ = {
   "VITE_TWITTER_URL": "https://x.com/zerofdn",
   "VITE_SEO_SITE_NAME": "ZeroFDN",
   "VITE_SEO_SITE_DESCRIPTION": "",
-  "VITE_SEO_SITE_URL": "https://zerofdn.xyz",
+  "VITE_SEO_SITE_URL": "https://dex.zerofdn.xyz",
   "VITE_SEO_SITE_LANGUAGE": "en",
   "VITE_SEO_SITE_LOCALE": "en_US",
   "VITE_SEO_TWITTER_HANDLE": "@ZeroFDN",
